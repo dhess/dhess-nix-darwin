ae4f473f1954ece1b6f6996a534300d27e0787d3
Drew Hess <src@drewhess.com>
2019-12-22 14:42:24 +0000

    Add modules to lib.

--- overlays/lib/dhess-nix-darwin.nix
+++ overlays/lib/dhess-nix-darwin.nix
@@ -7,11 +7,21 @@ let
   # Provide access to the whole package, if needed.
   path = ../../.;
 
+  # A list of all the nix-darwin modules exported by this package.
+  modulesList = ../../modules/module-list.nix;
+
+
+  # All nix-darwin modules exported by this package. To use, add this
+  # expression to your configuration's list of imports.
+  modules = import modulesList;
+
 in
 {
   lib = (super.lib or {}) // {
     dhess-nix-darwin = (super.lib.dhess-nix-darwin or {}) // {
       inherit path;
+      inherit modules modulesList;
+      inherit (localLibs) sources;
 
       # Access to dhess-nix-darwin's fixed nix-darwin.
       inherit (localLibs) nix-darwin;
