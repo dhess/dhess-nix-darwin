15205e5ede89d4e6edd98092a9d2a83d3ade2a80
Drew Hess <src@drewhess.com>
2020-02-24 23:11:19 +0000

    /writeFile/writeText/.

--- modules/config/remote-builds/build-host/default.nix
+++ modules/config/remote-builds/build-host/default.nix
@@ -41,7 +41,7 @@ let
     ''
   ) (mkHostPortPairs remoteBuildHosts);
 
-  sshConfig = pkgs.writeFile "ssh_config" sshExtraConfig;
+  sshConfig = pkgs.writeText "ssh_config" sshExtraConfig;
 
 in
 {
